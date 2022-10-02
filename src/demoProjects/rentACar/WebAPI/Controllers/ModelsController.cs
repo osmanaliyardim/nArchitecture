84f2bc3b347e2377c33b1bbe5cd1abb4d1775cde
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-10-02 14:51:27 +0300

    FEAT: CarModel Repository
    Repository, API and Paging implementation completed for car models.

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListQuery;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        //[HttpPost]
+        //public async Task<IActionResult> Add([FromBody] CreateModelCommand createModelCommand)
+        //{
+        //    CreatedModelDto result = await Mediator.Send(createModelCommand);
+
+        //    return Created("", result);
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new() { PageRequest = pageRequest };
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+
+            return Ok(result);
+        }
+    }
+}
